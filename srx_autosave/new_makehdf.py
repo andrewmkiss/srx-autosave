540318c9815bd4249b132f2e743f632826c12305
Andy Kiss <akiss@bnl.gov>
2021-05-18 14:51:42 -0400

    Changes for new metadata. Added step scan.

--- srx_autosave/new_makehdf.py
+++ srx_autosave/new_makehdf.py
@@ -130,10 +130,15 @@ def new_makehdf(scanid=-1, create_each_det=False):
 
     # Check for detectors
     dets = []
-    if 'xs' in h.start['scan']['detectors']:
-        dets.append('xs')
-    elif 'xs2' in h.start['scan']['detectors']:
-        dets.append('xs2')
+    try:
+        if 'xs' in h.start['scan']['detectors']:
+            dets.append('xs')
+        elif 'xs2' in h.start['scan']['detectors']:
+            dets.append('xs2')
+    except KeyError:
+        # AMK forgot to add detectors to step scans
+        if scan_doc['type'] == 'XRF_STEP':
+            dets.append('xs')
 
     if dets == []:
         print('No detectors found!')
@@ -155,7 +160,7 @@ def new_makehdf(scanid=-1, create_each_det=False):
         else:
             print(f'{fast_motor} not found!')
             return
-    
+
         slow_motor = scan_doc['slow_axis']['motor_name']
         if (slow_motor == 'nano_stage_sx'):
             slow_key = 'enc1'
@@ -165,7 +170,6 @@ def new_makehdf(scanid=-1, create_each_det=False):
             slow_key = 'enc3'
         else:
             slow_key = slow_motor
-    
     
         fast_pos = h.data(fast_key, stream_name='stream0', fill=True)
         fast_pos = np.array(list(fast_pos))
@@ -198,6 +202,7 @@ def new_makehdf(scanid=-1, create_each_det=False):
             N_xs2 = d_xs2.shape[2]
             d_xs2_sum = np.squeeze(np.sum(d_xs2, axis=2))
 
+        
         # Scaler list
         sclr_list = ['i0', 'i0_time', 'time', 'im', 'it']
         sclr = []
@@ -209,76 +214,144 @@ def new_makehdf(scanid=-1, create_each_det=False):
                 sclr_name.append(s)
         sclr = np.array(sclr)
         sclr = np.moveaxis(sclr, 0, -1)
+    if scan_doc['type'] == 'XRF_STEP':
+        # Define keys for motor data
+        fast_motor = scan_doc['fast_axis']['motor_name']
+        fast_key = fast_motor + '_user_setpoint'
+        slow_motor = scan_doc['slow_axis']['motor_name']
+        slow_key = slow_motor + '_user_setpoint'
+
+        # Collect motor positions
+        fast_pos = h.data(fast_key, stream_name='primary', fill=True)
+        fast_pos = np.array(list(fast_pos))
+        slow_pos = h.data(slow_key, stream_name='primary', fill=True)
+        slow_pos = np.array(list(slow_pos))
+
+        # Reshape motor positions
+        r, c = scan_doc['shape']
+        fast_pos = np.reshape(fast_pos, (r, c))
+        slow_pos = np.reshape(slow_pos, (r, c))
+
+        # Put into one array for h5 file
+        pos_pos = np.zeros((2, r, c))
+        if 'x' in slow_key:
+            pos_pos[1, :, :] = fast_pos
+            pos_pos[0, :, :] = slow_pos
+        else:
+            pos_pos[0, :, :] = fast_pos
+            pos_pos[1, :, :] = slow_pos
+        pos_name = ['x_pos', 'y_pos']
 
 
-        # Write file
-        interpath = 'xrfmap'
-        for d in dets:
-            if d == 'xs':
-                tmp_data = d_xs
-                tmp_data_sum = d_xs_sum
-                N = N_xs
-            elif d == 'xs2':
-                tmp_data = d_xs2
-                tmp_data_sum = d_xs2_sum
-                N = N_xs2
-
-            if (create_each_det):
-                fn = f'scan2D_{scanid}_{d}_{N}ch.h5'
+        # Get detector data
+        keys = h.table().keys()
+        MAX_DET_ELEMENTS = 7
+        for i in np.arange(1, MAX_DET_ELEMENTS+1):
+            if f'xs_channel{i}' in keys:
+                N_xs = i
             else:
-                fn = f'scan2D_{scanid}_{d}_sum{N}ch.h5'
+                break
+        N_pts = r * c
+        N_bins= 4096
+        if 'xs' in dets:
+            d_xs = np.empty((N_xs, N_pts, N_bins))
+            for i in np.arange(0, N_xs):
+                d = h.data(f'xs_channel{i+1}', fill=True)
+                d = np.array(list(d))
+                d_xs[i, :, :] = np.copy(d)
+            del d
+            # Reshape data
+            d_xs = np.reshape(d_xs, (N_xs, r, c, N_bins))
+            # Sum data
+            d_xs_sum = np.squeeze(np.sum(d_xs, axis=0))
+
+        # Scaler list
+        sclr_list = ['sclr_i0', 'sclr_im', 'sclr_it']
+        sclr_name = []
+        for s in sclr_list:
+            if s in keys:
+                sclr_name.append(s)
+        sclr = np.array(h.table()[sclr_name].values)
+        # sclr = np.moveaxis(sclr, 0, 1)
+        sclr = np.reshape(sclr, (r, c, len(sclr_name)))
+
+        # Consider snake
+        # pos_pos, d_xs, d_xs_sum, sclr
+        if scan_doc['snake'] == 1:
+            pos_pos[:, 1::2, :] = pos_pos[:, 1::2, ::-1]
+            d_xs[:, 1::2, :, :] = d_xs[:, 1::2, ::-1, :]
+            d_xs_sum[1::2, :, :] = d_xs_sum[1::2, ::-1, :]
+            sclr[1::2, :, :] = sclr[1::2, ::-1, :]
             
-            file_open_mode = 'a'
-            fname_add_version = True
-            file_overwrite_existing = False
-            if fname_add_version:
-                fpath = _get_fpath_not_existing(fn)
+
+    # Write file
+    interpath = 'xrfmap'
+    for d in dets:
+        if d == 'xs':
+            tmp_data = d_xs
+            tmp_data_sum = d_xs_sum
+            N = N_xs
+        elif d == 'xs2':
+            tmp_data = d_xs2
+            tmp_data_sum = d_xs2_sum
+            N = N_xs2
+ 
+        if (create_each_det):
+            fn = f'scan2D_{scanid}_{d}_{N}ch.h5'
+        else:
+            fn = f'scan2D_{scanid}_{d}_sum{N}ch.h5'
+        
+        file_open_mode = 'a'
+        fname_add_version = True
+        file_overwrite_existing = False
+        if fname_add_version:
+            fpath = _get_fpath_not_existing(fn)
+        else:
+            if file_overwrite_existing:
+                file_open_mode = 'w'
             else:
-                if file_overwrite_existing:
-                    file_open_mode = 'w'
-                else:
-                    print('File already exists!')
-                    return
-
-            with h5py.File(fn, file_open_mode) as f:
-                 # Create metadata group
-                metadata_grp = f.create_group(f"{interpath}/scan_metadata")
-                # This group of attributes are always created. It doesn't matter if metadata
-                #   is provided to the function.
-                metadata_grp.attrs["file_type"] = "XRF-MAP"
-                metadata_grp.attrs["file_format"] = "NSLS2-XRF-MAP"
-                metadata_grp.attrs["file_format_version"] = "1.0"
-                metadata_grp.attrs["file_software"] = "PyXRF"
-                metadata_grp.attrs["file_software_version"] = pyxrf_version
-                # Present time in NEXUS format (should it be UTC time)?
-                metadata_grp.attrs["file_created_time"] = ttime.strftime("%Y-%m-%dT%H:%M:%S+00:00", ttime.localtime())
-        
-                # Now save the rest of the scan metadata if metadata is provided
-                if mdata:
-                    # We assume, that metadata does not contain repeated keys. Otherwise the
-                    #   entry with the last occurrence of the key will override the previous ones.
-                    for key, value in mdata.items():
-                        metadata_grp.attrs[key] = value
-        
-                if create_each_det is True:
-                    for i in range(N_xs):
-                        grp = f.create_group(interpath+f'/det{i+1}')
-                        grp.create_dataset('counts', data=np.squeeze(tmp_data[:, :, i, :]), compression='gzip')
-
-                # summed data
-                dataGrp = f.create_group(interpath+'/detsum')
-                ds_data = dataGrp.create_dataset('counts', data=tmp_data_sum,
-                                                     compression='gzip')
-        
-                # add positions
-                dataGrp = f.create_group(interpath+'/positions')
-                dataGrp.create_dataset('name', data=helper_encode_list(pos_name))
-                dataGrp.create_dataset('pos', data=pos_pos)
-        
-                # scaler data
-                dataGrp = f.create_group(interpath+'/scalers')
-                dataGrp.create_dataset('name', data=helper_encode_list(sclr_name))
-                dataGrp.create_dataset('val', data=sclr)
+                print('File already exists!')
+                return
+ 
+        with h5py.File(fn, file_open_mode) as f:
+             # Create metadata group
+            metadata_grp = f.create_group(f"{interpath}/scan_metadata")
+            # This group of attributes are always created. It doesn't matter if metadata
+            #   is provided to the function.
+            metadata_grp.attrs["file_type"] = "XRF-MAP"
+            metadata_grp.attrs["file_format"] = "NSLS2-XRF-MAP"
+            metadata_grp.attrs["file_format_version"] = "1.0"
+            metadata_grp.attrs["file_software"] = "PyXRF"
+            metadata_grp.attrs["file_software_version"] = pyxrf_version
+            # Present time in NEXUS format (should it be UTC time)?
+            metadata_grp.attrs["file_created_time"] = ttime.strftime("%Y-%m-%dT%H:%M:%S+00:00", ttime.localtime())
+    
+            # Now save the rest of the scan metadata if metadata is provided
+            if mdata:
+                # We assume, that metadata does not contain repeated keys. Otherwise the
+                #   entry with the last occurrence of the key will override the previous ones.
+                for key, value in mdata.items():
+                    metadata_grp.attrs[key] = value
+    
+            if create_each_det is True:
+                for i in range(N_xs):
+                    grp = f.create_group(interpath+f'/det{i+1}')
+                    grp.create_dataset('counts', data=np.squeeze(tmp_data[:, :, i, :]), compression='gzip')
+ 
+            # summed data
+            dataGrp = f.create_group(interpath+'/detsum')
+            ds_data = dataGrp.create_dataset('counts', data=tmp_data_sum,
+                                                 compression='gzip')
+    
+            # add positions
+            dataGrp = f.create_group(interpath+'/positions')
+            dataGrp.create_dataset('name', data=helper_encode_list(pos_name))
+            dataGrp.create_dataset('pos', data=pos_pos)
+    
+            # scaler data
+            dataGrp = f.create_group(interpath+'/scalers')
+            dataGrp.create_dataset('name', data=helper_encode_list(sclr_name))
+            dataGrp.create_dataset('val', data=sclr)
 
 
 def add_ydata(fn):
