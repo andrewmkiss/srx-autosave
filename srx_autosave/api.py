7888bda7612c7bc70eb2290d94c84f53f1604866
Andy Kiss <akiss@bnl.gov>
2024-03-10 21:26:15 -0400

    Untested. Added export XAS functionality

--- srx_autosave/api.py
+++ srx_autosave/api.py
@@ -53,7 +53,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.WARNING)
 
 #FLAG for auto_roi and create_pdf
-auto_roi_flag = True
+auto_roi_flag = False
 """
 SRX Autosave APIs
 
@@ -373,6 +373,209 @@ def add_encoder_data(scanid):
         print(f'Error writing to file: {fn}')
 
 
+def xanes_textout(scan=-1, header=[], userheader={}, column=[], usercolumn={},
+                  usercolumnname=[], output=True, filename_add='', filedir=None):
+    '''
+    scan: can be scan_id (integer) or uid (string). default = -1 (last scan run)
+    header: a list of items that exist in the event data to be put into the header
+    userheader: a dictionary defined by user to put into the hdeader
+    column: a list of items that exist in the event data to be put into the column data
+    output: print all header fileds. if output = False, only print the ones that were able to be written
+            default = True
+
+    '''
+    if (filedir is None):
+        filedir = userdatadir
+    h = db[scan]
+    # get events using fill=False so it does not look for the metadata in filestorage with reference (hdf5 here)
+    events = [document for name, document
+              in db.get_documents(h, fill=False, stream_name="primary")
+              if name=="event"]
+
+    if (filename_add != ''):
+        filename = 'scan_' + str(h.start['scan_id']) + '_' + filename_add
+    else:
+        filename = 'scan_' + str(h.start['scan_id'])
+
+    with open(filedir+filename, 'w') as f:
+
+        staticheader = '# XDI/1.0 MX/2.0\n' \
+                  + '# Beamline.name: ' + h.start['beamline_id'] + '\n' \
+                  + '# Facility.name: NSLS-II\n' \
+                  + '# Facility.ring_current:' + str(events[0]['data']['ring_current']) + '\n' \
+                  + '# Scan.start.uid: ' + h.start['uid'] + '\n' \
+                  + '# Scan.start.time: '+ str(h.start['time']) + '\n' \
+                  + '# Scan.start.ctime: ' + ttime.ctime(h.start['time']) + '\n' \
+                  + '# Mono.name: Si 111\n'
+
+        f.write(staticheader)
+
+        for item in header:
+            if (item in events[0].data.keys()):
+                f.write('# ' + item + ': ' + str(events[0]['data'][item]) + '\n')
+                if (output is True):
+                    print(item + ' is written')
+            else:
+                print(item + ' is not in the scan')
+
+        for key in userheader:
+            f.write('# ' + key + ': ' + str(userheader[key]) + '\n')
+            if (output is True):
+                print(key + ' is written')
+
+        for idx, item in enumerate(column):
+            if (item in events[0].data.keys()):
+                f.write('# Column.' + str(idx+1) + ': ' + item + '\n')
+
+        f.write('# ')
+        for item in column:
+            if (item in events[0].data.keys()):
+                f.write(str(item) + '\t')
+
+        for item in usercolumnname:
+            f.write(item + '\t')
+
+        f.write('\n')
+        f.flush()
+
+        idx = 0
+        for event in events:
+            for item in column:
+                if (item in events[0].data.keys()):
+                    f.write('{0:8.6g}  '.format(event['data'][item]))
+            for item in usercolumnname:
+                try:
+                    f.write('{0:8.6g}  '.format(usercolumn[item][idx]))
+                except KeyError:
+                    idx += 1
+                    f.write('{0:8.6g}  '.format(usercolumn[item][idx]))
+            idx = idx + 1
+            f.write('\n')
+
+
+
+
+def xanes_afterscan_plan(scanid, filename, roinum):
+    # Custom header list
+    headeritem = []
+    # Load header for our scan
+    h = db[scanid]
+
+    # Construct basic header information
+    userheaderitem = {}
+    userheaderitem['uid'] = h.start['uid']
+    userheaderitem['sample.name'] = h.start['scan']['sample_name']
+    # userheaderitem['initial_sample_position.hf_stage.x'] = h.start['initial_sample_position']['hf_stage_x']
+    # userheaderitem['initial_sample_position.hf_stage.y'] = h.start['initial_sample_position']['hf_stage_y']
+    # userheaderitem['hfm.y'] = h.start['hfm']['y']
+    # userheaderitem['hfm.bend'] = h.start['hfm']['bend']
+
+    # Create columns for data file
+    columnitem = ['energy_energy', 'energy_bragg', 'energy_c2_x']
+    # Include I_M, I_0, and I_t from the SRS
+    if ('sclr1' in h.start['detectors']):
+        if 'sclr_i0' in h.table('primary').keys():
+            columnitem = columnitem + ['sclr_im', 'sclr_i0', 'sclr_it']
+        else:
+            columnitem = columnitem + ['sclr1_mca3', 'sclr1_mca2', 'sclr1_mca4']
+
+    else:
+        raise KeyError("SRS not found in data!")
+    # Include fluorescence data if present, allow multiple rois
+    if ('xs' in h.start['detectors']):
+        if (type(roinum) is not list):
+            roinum = [roinum]
+        print(roinum)
+        for i in roinum:
+            print(i)
+            roi_name = 'roi{:02}'.format(i)
+            # JL is this correct?
+            roi_key = []
+            # JL what is .value.name with the community xspress3 IOC?
+            #roi_key.append(getattr(xs.channel1.rois, roi_name).value.name)
+            #roi_key.append(getattr(xs.channel2.rois, roi_name).value.name)
+            #roi_key.append(getattr(xs.channel3.rois, roi_name).value.name)
+            #roi_key.append(getattr(xs.channel4.rois, roi_name).value.name)
+            for xs_channel in xs.iterate_channels():
+                print(xs_channel.name)
+                roi_key.append(
+                    xs_channel.get_mcaroi(mcaroi_number=i).total_rbv.name
+                )
+                # roi_key.append(
+                #     xs_channel.get_mcaroi(mcaroi_number=roi_name).total_rbv.name
+                # )
+
+        [columnitem.append(roi) for roi in roi_key]
+    if ('xs2' in h.start['detectors']):
+        if (type(roinum) is not list):
+            roinum = [roinum]
+        for i in roinum:
+            roi_name = 'roi{:02}'.format(i)
+            roi_key = []
+            roi_key.append(getattr(xs2.channel1.rois, roi_name).value.name)
+
+        [columnitem.append(roi) for roi in roi_key]
+    # Construct user convenience columns allowing prescaling of ion chamber, diode and
+    # fluorescence detector data
+    usercolumnitem = {}
+    datatablenames = []
+
+    if ('xs' in h.start['detectors']):
+        datatablenames = datatablenames + [str(roi) for roi in roi_key]
+    if ('xs2' in h.start['detectors']):
+        datatablenames = datatablenames + [str(roi) for roi in roi_key]
+    if ('sclr1' in  h.start['detectors']):
+        if 'sclr_im' in h.table(stream_name='primary').keys():
+            datatablenames = datatablenames + ['sclr_im', 'sclr_i0', 'sclr_it']
+            datatable = h.table(stream_name='primary', fields=datatablenames)
+            im_array = np.array(datatable['sclr_im'])
+            i0_array = np.array(datatable['sclr_i0'])
+            it_array = np.array(datatable['sclr_it'])
+        else:
+            datatablenames = datatablenames + ['sclr1_mca2', 'sclr1_mca3', 'sclr1_mca4']
+            datatable = h.table(stream_name='primary', fields=datatablenames)
+            im_array = np.array(datatable['sclr1_mca3'])
+            i0_array = np.array(datatable['sclr1_mca2'])
+            it_array = np.array(datatable['sclr1_mca4'])
+    else:
+        raise KeyError
+    # Calculate sums for xspress3 channels of interest
+    if ('xs' in h.start['detectors']):
+        for i in roinum:
+            roi_name = 'roi{:02}'.format(i)
+            # JL (again) what is .value.name for the community xspress3 IOC?
+            # roisum = datatable[getattr(xs.channel1.rois, roi_name).value.name]
+            # roisum = roisum + datatable[getattr(xs.channel2.rois, roi_name).value.name]
+            # roisum = roisum + datatable[getattr(xs.channel3.rois, roi_name).value.name]
+            # roisum = roisum + datatable[getattr(xs.channel4.rois, roi_name).value.name]
+            roisum = sum(
+                [
+                    datatable[
+                        xs_channel.get_mcaroi(mcaroi_number=i).total_rbv.name
+                    ]
+                    # datatable[
+                    #     xs_channel.get_mcaroi(mcaroi_number=roinum).total_rbv.name
+                    # ]
+                    for xs_channel
+                    in xs.iterate_channels()
+                ]
+            )
+            usercolumnitem['If-{:02}'.format(i)] = roisum
+            usercolumnitem['If-{:02}'.format(i)].round(0)
+    if ('xs2' in h.start['detectors']):
+        for i in roinum:
+            roi_name = 'roi{:02}'.format(i)
+            roisum = datatable[getattr(xs2.channel1.rois, roi_name).value.name]
+            usercolumnitem['If-{:02}'.format(i)] = roisum
+            usercolumnitem['If-{:02}'.format(i)].round(0)
+
+    xanes_textout(scan = scanid, header = headeritem,
+                  userheader = userheaderitem, column = columnitem,
+                  usercolumn = usercolumnitem,
+                  usercolumnname = usercolumnitem.keys(),
+                  output = False, filename_add = filename, filedir=userdatadir)
+
+
 def xrf_loop(start_id, N, gui=None):
     auto_dir = "auto_rois/"
     num = np.arange(start_id, start_id + N, 1)
@@ -430,9 +633,23 @@ def xrf_loop(start_id, N, gui=None):
                     pass
             else:   
                 print(f"XRF HDF5 already created.")
-
         elif h.start['scan']['type'] == 'XAS_STEP':
-            print("Let's do this!")
+            # Check if file exists
+            if not glob.glob(f"scan{scanid}*") and not os.path.isfile(f"scan{scanid}.xdi"):
+                # Check if scan is done
+                try:
+                    db[scanid].stop['time']
+                    # Make file
+                    # xanes_afterscan_plan(scanid, filename, roinum)
+                    xanes_afterscan_plan(scanid, f"scan{scanid}.xdi", [1])
+                except KeyError:
+                    print("Scan not complete...")
+                    pass
+                except Exception:
+                    traceback.print_exc()
+                    pass
+            else:
+                print("XDI already created")
         else:
             print()
 
